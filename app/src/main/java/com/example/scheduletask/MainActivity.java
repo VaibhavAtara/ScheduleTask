aba9f4f4de3783de27a14d26802dc95571795bf6
Vaibhav <vaibhavatara@example.com>
2020-02-19 16:49:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/scheduletask/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.scheduletask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    TimePicker timePicker;
+    Button setAlarm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timePicker = (TimePicker)findViewById(R.id.timepicker);
+        setAlarm = (Button)findViewById(R.id.setAlarm);
+
+        setAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Calendar calendar = Calendar.getInstance();
+                if(Build.VERSION.SDK_INT >= 23) {
+                    calendar.set(calendar.get(Calendar.YEAR),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH),
+                            timePicker.getHour(),
+                            timePicker.getMinute(),
+                            0);
+                }else{
+                    calendar.set(calendar.get(Calendar.YEAR),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH),
+                            timePicker.getCurrentHour(),
+                            timePicker.getCurrentMinute(),
+                            0);
+                }
+
+                SetAlarm(calendar.getTimeInMillis());
+
+            }
+        });
+    }
+
+    private void SetAlarm(long timeInMillis) {
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this,PublishMessage.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,0,intent,0);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,timeInMillis,AlarmManager.INTERVAL_DAY,pendingIntent);
+
+        Toast.makeText(getApplicationContext(),"Alarm is Set",Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/scheduletask/PublishMessage.java
@@ -0,0 +1,17 @@
+package com.example.scheduletask;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+import android.widget.Toast;
+
+public class PublishMessage extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+        mediaPlayer.start();
+        Toast.makeText(context,"IN ALARM",Toast.LENGTH_SHORT).show();
+    }
+}
